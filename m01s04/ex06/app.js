47c88f4f039e516171e69520b36aa505bfe0cb3a
AlexandraM07 <110460669+AlexandraM07@users.noreply.github.com>
2022-12-06 16:20:52 +0200

    JS s04

---
+++ m01s04/ex06/app.js
@@ -0,0 +1,33 @@
+var person = {
+  name: 'Dragos',
+  surname: 'Iordache',
+  age: 35,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquerry',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 31,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
